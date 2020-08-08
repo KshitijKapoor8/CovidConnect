d325df2c792febff135a4bc1617c32e290d1c20c
LeftClik <samrut.gadde@gmail.com>
2020-08-08 16:24:30 -0500

    react basics

---
+++ covidconnectclient/src/pages/connect.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class connect extends Component {
+    render() {
+        return (
+            <div>
+                 <h1>CONNECT SCREEN</h1>
+            </div>
+        )
+    }
+}
+
+export default connect
---
+++ covidconnectclient/src/pages/learn.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class learn extends Component {
+    render() {
+        return (
+            <div>
+                <h1>learn SCREEN</h1>
+            </div>
+        )
+    }
+}
+
+export default learn
