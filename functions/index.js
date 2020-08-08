5dca161e530b5b3536642b2198e330bf432e6307
Kshitij Kapoor <kshitijkapoor8@gmail.com>
2020-08-08 14:38:09 -0500

    Step 2

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,43 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const express = require('express');
+const app = express();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello world!");
+admin.initializeApp();
+
+app.get('/posts', (req, res) => {
+    admin
+        .firestore()
+        .collection("posts")
+        .get()
+            .then((data) => {
+                let posts = [];
+                data.forEach((doc) => {
+                    posts.push(doc.data());
+                });
+                return res.json(posts)
+            })
+        .catch(err => console.error(err));
 });
+
+app.post('/make', (req, res) => {
+    const newPost = {
+        body: req.body.body,
+        date: admin.firestore.Timestamp.fromDate(new Date()),
+        userHandle: req.body.userHandle
+    };
+
+    admin.firestore()
+        .collection("posts")
+        .add(newPost)
+        .then((doc) => {
+            res.json({ message : `document ${doc.id} created succesfully`});
+        })
+        .catch((err) => {
+            res.status(500).json({ error : 'Something went wrong'});
+            console.error(err)
+        })
+});
+
+
+exports.api = functions.https.onRequest(app);
