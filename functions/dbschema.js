de9b6dd4b350be37956d4085c0652a767007df15
Kshitij Kapoor <kshitijkapoor8@gmail.com>
2020-08-08 15:10:37 -0500

    t

---
+++ functions/dbschema.js
@@ -0,0 +1,10 @@
+let db = {
+    posts: [
+        {
+            userHandle: 'user',
+            body: 'this is the scream body',
+            date: '2020-08-08T19:57:03.402Z',
+            
+        }
+    ]
+}
