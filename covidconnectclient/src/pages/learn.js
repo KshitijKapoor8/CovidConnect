953f0a970358d3fddc617b7d97090cf02fa5383b
Kshitij Kapoor <kshitijkapoor8@gmail.com>
2020-08-09 14:02:19 -0500

    learn frame

--- covidconnectclient/src/pages/learn.js
+++ covidconnectclient/src/pages/learn.js
@@ -1,21 +1,29 @@
 import React, { Component } from "react";
 import axios from 'axios';
 
+let number = 0;
+let number2 = 0;
 export class learn extends Component {
-  render() {
-
-    axios.get('https://covid19.mathdro.id/api/confirmed')
-    .then( (res) => {
-        console.log(res);
+  
+    render() {
+      axios.get('https://covid19.mathdro.id/api')
+      .then( (res) => {
+        number = res.data.confirmed.value;
+        number2 = res.data.deaths.value;
+        console.log(number)
+        return number;
+      })
+      .catch(err => {
+        console.log(err);
     })
     
     return (
+      console.log(number),
       <div>
-        <h1>Coronavirus Tracker</h1>
-        <h2>Unfortunately, the disease has caused much havoc</h2>
-        
+        <h1 class = "test1">These many people have suffered-</h1>
+        <h1 class = "test">{number} people infected.</h1>
+        <h1 class = "test">{number2} deaths.</h1>
       </div>
-
     )
   }
 }
